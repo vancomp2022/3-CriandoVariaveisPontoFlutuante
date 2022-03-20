636a9582fcb98558ea2e27f94d05a27df6ee9141
Vanessa Soares de Morais <vsmdantas@outlook.com>
2022-03-20 08:52:35 -0300

    Adicionar arquivos de projeto.

---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario;
+            salario = 1200.70;
+
+            Console.WriteLine(salario);
+
+            double idade;
+           // idade = 15 / 2.0;
+          //  Console.WriteLine(idade);
+
+           // idade = 5 / 3;
+          //  Console.WriteLine("5 / 3 =" + idade);
+
+            idade = 5.0 / 3;
+            Console.WriteLine("5.0 /3 =" + idade);
+            Console.WriteLine("Executando Projeto 3 Criando Variaveis Ponto Flutuante");
+            Console.WriteLine("A execucao acabou. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
